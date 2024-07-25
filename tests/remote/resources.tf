aba3325ea7ea6d043347130d632bd49d49c086c9
Roman Schwarz <rs@cloudeteer.de>
2024-07-25 16:30:50 +0200

    Configure TFLint
    - Add azurerm plugin
    - Add separate config for examples
    - Add separate pre-commit hook for examples
    - Fix TFLint warnings
    
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- tests/remote/resources.tf
+++ tests/remote/resources.tf
@@ -6,6 +6,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.111"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
