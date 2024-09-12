724655f6f43e620ccc5f9df357c1fcce84df7acb
Jan-Otto Kröpke <github@jkroepke.de>
2024-09-12 10:30:26 +0200

    fix: relax version constrains
    Signed-off-by: Jan-Otto Kröpke <github@jkroepke.de>

--- versions.tf
+++ versions.tf
@@ -1,25 +1,25 @@
 terraform {
-  required_version = "~> 1.9"
+  required_version = ">= 1.9"
 
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "~> 1.14"
+      version = ">= 1.14"
     }
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.111"
+      version = ">= 3.111"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
 }
