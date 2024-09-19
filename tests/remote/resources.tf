59e3ec8b64d3879fcf35b5c72c7fe9887182a89b
Roman Schwarz <rs@cloudeteer.de>
2024-09-19 22:24:17 +0200

    Remove default features
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- tests/remote/resources.tf
+++ tests/remote/resources.tf
@@ -25,12 +25,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+  features {}
 }
 
 #
