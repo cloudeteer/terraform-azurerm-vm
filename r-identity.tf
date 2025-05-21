960962a524b8ee159a7da1debd6ab053155d2c0d
Roman Schwarz <rswrz@users.noreply.github.com>
2025-05-21 07:04:14 +0200

    Fix incorrect `role_definition_id` usage in `azurerm_role_assignment` (#78)
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- r-identity.tf
+++ r-identity.tf
@@ -17,8 +17,7 @@ resource "azurerm_user_assigned_identity" "this" {
 resource "azurerm_role_assignment" "entra_id_login" {
   for_each = var.entra_id_login.enabled ? toset(var.entra_id_login.principal_ids) : []
 
-  principal_id = each.value
-  scope = (local.is_linux ? azurerm_linux_virtual_machine.this[0].id :
-  azurerm_windows_virtual_machine.this[0].id)
-  role_definition_id = "Virtual Machine Administrator Login"
+  principal_id         = each.value
+  role_definition_name = "Virtual Machine Administrator Login"
+  scope                = local.virtual_machine.id
 }
